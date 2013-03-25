66fc4156c7e1dc10bacd00abe42aa2a1d2654748
hit9 <nz2324@126.com>
2013-03-25 21:48:16 +0800

    escape for string literal

--- toooml.py
+++ toooml.py
@@ -1,8 +1,14 @@
-#coding=utf8
+# coding=utf8
 from ply import lex
 from ply import yacc
+from ply.lex import TOKEN
 from datetime import datetime
 from re import UNICODE
+
+# escape string
+ES = r"""(\\(['"\?\\abfnrtv]|[0-7]{1,3}|x[a-fA-F0-9]+))"""
+
+STR = r'\"([^"\\\n]|'+ES+')*\"'
 
 class TomlSyntaxError(SyntaxError):
     pass
@@ -28,13 +34,16 @@ t_ignore = "\x20\x09"
 
 t_ignore_COMMENT = r'\#.*'
 
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
 def t_error(t):
     raise TomlSyntaxError(
-        u"Illegal character '{0}' at line {1}".format(t.value[0], t.lexer.lineno)
+        u"Illegal character '{0}' at line {1}".format(
+            t.value[0], t.lexer.lineno)
     )
 
 
@@ -42,22 +51,26 @@ def t_error(t):
 t_EQUALS = r"="
 
 # function tokens
+
+
 def t_KEY(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     return t
+
 
 def t_KEYGROUP(t):
     r'\[([a-zA-Z_][a-zA-Z0-9_]*\.?)+\]'
     t.value = tuple(t.value[1:-1].split('.'))
     return t
 
+
 def t_DATETIME(t):
     r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z'
     t.value = datetime.strptime(t.value, ISO8601)
     return t
 
+@TOKEN(STR)
 def t_STRING(t):
-    r'[a-zA-Z_]?\"(\\.|[^\\"])*\"'
     t.value = t.value[1:-1]
     return t
 
@@ -66,10 +79,12 @@ def t_FLOAT(t):
     t.value = float(t.value)
     return t
 
+
 def t_INTEGER(t):
     r'\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
     t.value = int(t.value)
     return t
+
 
 def t_BOOLEN(t):
     r'true|false'
@@ -84,5 +99,6 @@ text = open("test.toml").read().decode("utf8")
 lex.input(text)
 while 1:
     tok = lex.token()
-    if not tok: break
+    if not tok:
+        break
     print tok.value
